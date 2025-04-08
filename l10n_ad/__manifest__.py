1bc7ad5e837e9f732fc0c6caf28044150bd06767
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-04-08 15:49:09 +0200

    l10n_ad: correccions menors a manifest (cometes, majúscules, espais)

--- l10n_ad/__manifest__.py
+++ l10n_ad/__manifest__.py
@@ -1,26 +1,26 @@
 {
     'name': 'Andorra - Accounting',
-    'summary': '""Creació de grups comptables, Pla General Comptable i taxes Andorranes (IGI, IRPF)""',
+    'summary': 'Creació de grups comptables, Pla General Comptable i taxes andorranes (IGI, IRPF)',
     'version': '1.0',
     'icon': '/account/static/description/l10n.png',
     'countries': ['ad'],
     'author': 'Batista10',
-    'website': 'https://batista10.cat',    
+    'website': 'https://batista10.cat',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
-Andorra Comptes Comptables 
+Andorra Comptes Comptables
 ==========================
 
     * Creació de grups comptables
     * Creació del Pla General Comptable
-    * Creació de taxes Andorranes (IGI, IRPF)
+    * Creació de taxes andorranes (IGI, IRPF)
 """,
     'depends': [
         'account',
         'base_iban',
         'base_vat',
     ],
-    'data': [        
+    'data': [
         'data/res_partner_data.xml',
     ],
     'license': 'LGPL-3',
