d4f7ccb845a6a37f3cf13aab350de376a2adb178
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-06-06 12:23:51 +0200

    l10n_ad: ordenació alfabètica de les importacions de Python

--- l10n_ad/models/account_move.py
+++ l10n_ad/models/account_move.py
@@ -1,5 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class AccountMove(models.Model):
