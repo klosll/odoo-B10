ff3ca9257054c808b3cd317a89025797c1388625
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-06-17 11:49:14 +0200

    l10n_ad: nota de copyright obsoleta en mòdul Python

--- l10n_ad/__init__.py
+++ l10n_ad/__init__.py
@@ -1,2 +1,3 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# Copyright 2024, 2025 Batista10
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 from . import models
